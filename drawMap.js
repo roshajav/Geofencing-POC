1d4fc6804fdda377bf9b42e433a290ceba08c86a
javidroshan <javid311@gmail.com>
2024-03-19 10:11:25 +0530

    + Changes related to adding low and medium risk areas.

--- drawMap.js
+++ drawMap.js
@@ -4,13 +4,15 @@ var bounds = new google.maps.LatLngBounds();
 const workOrders = document.querySelectorAll(".item");
 var geocoder;
 var polygons = [];
+var flagged = 0;
 var directionsRenderer, directionsService;
 var polygonData = JSON.parse(localStorage.getItem("polygonData")) || [
   [
-    { lat: 12.994007, lng: 77.705056 },
-    { lat: 12.993286, lng: 77.705089 },
-    { lat: 12.993192, lng: 77.705947 },
-    { lat: 12.994049, lng: 77.705829 },
+    { lat: 12.997489, lng: 77.707143 },
+    { lat: 12.994813, lng: 77.695985 },
+    { lat: 12.985446, lng: 77.702165 },
+    { lat: 12.990798, lng: 77.703195 },
+    { lat: 12.992722, lng: 77.712121 },
   ],
   [
     { lat: 13.039981, lng: 77.718195 },
@@ -19,17 +21,44 @@ var polygonData = JSON.parse(localStorage.getItem("polygonData")) || [
     { lat: 13.039813, lng: 77.733816 },
   ],
   [
-    { lat: 12.99058, lng: 77.710867 },
-    { lat: 12.987485, lng: 77.710867 },
-    { lat: 12.990161, lng: 77.716403 },
+    { lat: 12.979684, lng: 77.713832 },
+    { lat: 12.986543, lng: 77.723445 },
+    { lat: 12.97935, lng: 77.730827 },
+    { lat: 12.961283, lng: 77.711257 },
   ],
 ];
+
+var circleData = [
+  { center: { lat: 12.950984, lng: 77.635489 }, radius: 1000 },
+  { center: { lat: 13.019723, lng: 77.695432 }, radius: 800 },
+  { center: { lat: 12.985678, lng: 77.680987 }, radius: 1200 },
+];
+
+var rectangleData = [
+  [
+    { lat: 12.965482, lng: 77.645825 },
+    { lat: 12.965482, lng: 77.656389 },
+    { lat: 12.953672, lng: 77.656389 },
+    { lat: 12.953672, lng: 77.645825 },
+    { lat: 12.965482, lng: 77.645825 },
+  ],
+  [
+    { lat: 12.958765, lng: 77.678987 },
+    { lat: 12.958765, lng: 77.689012 },
+    { lat: 12.947123, lng: 77.689012 },
+    { lat: 12.947123, lng: 77.678987 },
+    { lat: 12.958765, lng: 77.678987 },
+  ],
+];
+
 var coordinates = [];
 let new_coordinates = [];
 let lastElement;
 
-function savePolygonDataToLocalStorage() {
+function saveGeoFenceDataToLocalStorage() {
   localStorage.setItem("polygonData", JSON.stringify(polygonData));
+  localStorage.setItem("circleData", JSON.stringify(circleData));
+  localStorage.setItem("rectangleData", JSON.stringify(rectangleData));
 }
 
 function InitMap() {
@@ -96,6 +125,56 @@ function InitMap() {
     }
   }
 
+  function addCircle(circle) {
+    var circleObject = new google.maps.Circle({
+      strokeColor: "#fcfc03",
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: "#fcfc03",
+      fillOpacity: 0.5,
+      map: map,
+      center: circle.center,
+      radius: circle.radius,
+      clickable: true,
+      draggable: true,
+      editable: true,
+    });
+  }
+
+  function addRectangle(rectangle) {
+    var rectangleObject = new google.maps.Rectangle({
+      strokeColor: "#f73d0a",
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: "#f73d0a",
+      fillOpacity: 0.5,
+      map: map,
+      bounds: {
+        north: rectangle[0].lat,
+        south: rectangle[2].lat,
+        east: rectangle[1].lng,
+        west: rectangle[3].lng,
+      },
+      clickable: true,
+      draggable: true,
+      editable: true,
+    });
+  }
+
+  function populateDefaultCircles() {
+    circleData.forEach(function (circle) {
+      addCircle(circle);
+    });
+  }
+
+  function populateDefaultRectangles() {
+    rectangleData.forEach(function (rectangle) {
+      addRectangle(rectangle);
+    });
+  }
+
+  populateDefaultCircles();
+  populateDefaultRectangles();
   populateDefaultPolygons();
 
   var drawingManager = new google.maps.drawing.DrawingManager({
@@ -109,26 +188,38 @@ function InitMap() {
       ],
     },
     circleOptions: {
-      fillColor: "#ffff00",
-      fillOpacity: 0.2,
-      strokeWeight: 3,
+      fillColor: "#fcfc03",
+      fillOpacity: 0.5,
+      strokeColor: "#fcfc03",
+      strokeWeight: 2,
+      strokeOpacity: 0.8,
       clickable: true,
       editable: true,
+      draggable: true,
       zIndex: 1,
+      id: Math.random().toString(16).slice(2),
     },
     polygonOptions: {
+      fillColor: "#e80707",
+      fillOpacity: 0.5,
+      strokeColor: "#e80707",
+      strokeWeight: 2,
+      strokeOpacity: 0.8,
       clickable: true,
       draggable: true,
       editable: true,
-      fillColor: "#ADFF2F",
-      fillOpacity: 0.5,
+      id: Math.random().toString(16).slice(2),
     },
     rectangleOptions: {
+      fillColor: "#f73d0a",
+      fillOpacity: 0.5,
+      strokeColor: "#f73d0a",
+      strokeWeight: 2,
+      strokeOpacity: 0.8,
       clickable: true,
       draggable: true,
       editable: true,
-      fillColor: "#ffff00",
-      fillOpacity: 0.5,
+      id: Math.random().toString(16).slice(2),
     },
   });
 
@@ -155,7 +246,7 @@ function InitMap() {
       drawingManager.setMap(map);
       coordinates.splice(0, coordinates.length);
       document.getElementById("info").innerHTML = "";
-      savePolygonDataToLocalStorage();
+      saveGeoFenceDataToLocalStorage();
     }
   }
 
@@ -197,7 +288,7 @@ function InitMap() {
       coordinates.push(newShape.getPath().getAt(i).toUrlValue(6));
     }
     document.getElementById("info").innerHTML = coordinates;
-    savePolygonDataToLocalStorage();
+    saveGeoFenceDataToLocalStorage();
   };
 
   google.maps.event.addListener(
@@ -215,6 +306,40 @@ function InitMap() {
         getPolygonCoords(event);
       });
       polygons.push(event);
+    }
+  );
+
+  google.maps.event.addListener(
+    drawingManager,
+    "circlecomplete",
+    function (circle) {
+      circle.setMap(null);
+      circleData.push({
+        center: {
+          lat: circle.getCenter().lat(),
+          lng: circle.getCenter().lng(),
+        },
+        radius: circle.getRadius(),
+      });
+      populateDefaultCircles();
+    }
+  );
+
+  google.maps.event.addListener(
+    drawingManager,
+    "rectanglecomplete",
+    function (rectangle) {
+      rectangle.setMap(null);
+      const bounds = rectangle.getBounds();
+      const ne = bounds.getNorthEast();
+      const sw = bounds.getSouthWest();
+      rectangleData.push([
+        { lat: ne.lat(), lng: sw.lng() },
+        { lat: ne.lat(), lng: ne.lng() },
+        { lat: sw.lat(), lng: ne.lng() },
+        { lat: sw.lat(), lng: sw.lng() },
+      ]);
+      populateDefaultRectangles();
     }
   );
 
@@ -251,8 +376,41 @@ function checkLocationInHazardousArea(location) {
     const polygon = polygons[i];
     if (google.maps.geometry.poly.containsLocation(location, polygon)) {
       alert("You are in a hazardous area! Please move away!");
-      break;
+      flagged = 1;
+      return;
     }
+  }
+
+  for (let i = 0; i < circleData.length; i++) {
+    const circle = circleData[i];
+    const center = new google.maps.LatLng(circle.center.lat, circle.center.lng);
+    const distance = google.maps.geometry.spherical.computeDistanceBetween(
+      center,
+      location
+    );
+    if (distance <= circle.radius) {
+      alert("You are in a low risk area. Please be cautious!");
+      flagged = 1;
+      return;
+    }
+  }
+
+  for (let i = 0; i < rectangleData.length; i++) {
+    const rectangle = rectangleData[i];
+    const bounds = new google.maps.LatLngBounds(
+      new google.maps.LatLng(rectangle[2].lat, rectangle[3].lng),
+      new google.maps.LatLng(rectangle[0].lat, rectangle[1].lng)
+    );
+    if (bounds.contains(location)) {
+      alert("You are in a medium risk area. Please be cautious!");
+      flagged = 1;
+      return;
+    }
+  }
+
+  if (flagged == 1) {
+    alert("You are in safe zone now!");
+    flagged = 0;
   }
 }
 
